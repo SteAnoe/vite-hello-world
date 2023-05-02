1dc27958eff43c61e9acf3526161de778a33fb82
SteAnoe <ste.anoe@gmail.com>
2023-05-02 13:37:36 +0200

    inizio struttura

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import AppTitle from './components/AppTitle.vue'
+export default{
+  name: "App",
+  components: {
+    AppTitle,
+    
+  }
+}
+</script>
+
+<template>
+  <div>
+    
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<script>
+
+  export default{
+    name: "AppTitle",
+    data() {
+        return {
+            
+        }
+    }
+}
+
+</script>
+
+<template>
+  <div>
+    Hello World!
+  </div>
+</template>
+
+<style scoped>
+
+</style>
